24eee9eb16f5e289fd31ade789e0d0bdc6cb6061
GvictorFranca <57845536+GvictorFranca@users.noreply.github.com>
2020-08-01 20:51:20 -0300

    test: ensure SignUpController return 400 status code if no email has provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,7 +5,6 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
@@ -14,5 +13,21 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing Param : name')) // To equal serve para comparar valores, enquanto toBe compara ponteiros
+  })
+})
+
+describe('SignUp Controller', () => {
+  test('Should return 400, if no email is provide', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing Param : email')) // To equal serve para comparar valores, enquanto toBe compara ponteiros
   })
 })
