72149741d6ceb327cb25033001c639524a71e244
GvictorFranca <57845536+GvictorFranca@users.noreply.github.com>
2020-08-02 00:41:06 -0300

    test: ensure SignUpController returns 400 if no passwordConfirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,7 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { SignUpController } from './signup'
 
+// SignUpController
 describe('SignUp Controller', () => {
   test('Should return 400, if no name is provide', () => {
     const sut = new SignUpController()
@@ -46,5 +47,21 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password')) // To equal serve para comparar valores, enquanto toBe compara ponteiros
+  })
+})
+
+describe('SignUp Controller', () => {
+  test('Should return 400, if no password confirmation is provide', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation')) // To equal serve para comparar valores, enquanto toBe compara ponteiros
   })
 })
